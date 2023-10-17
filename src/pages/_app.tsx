edb39bfb70aac4c77e17cfa271bcfb91497062aa
Caio Cezar <caiocezartg@gmail.com>
2023-10-17 20:03:48 -0300

    fix: use next/font to optimize Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { globalStyle } from "@/styles/globals";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
