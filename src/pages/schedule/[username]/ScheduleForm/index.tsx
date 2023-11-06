bfa31c157bf095042cc2bb3c80de4b2e7db5d9d2
Caio Cezar <caiocezartg@gmail.com>
2023-11-06 16:02:13 -0300

    feat: add schedule confirmation page

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CalendarStep from "./CalendarStep";
+import ConfirmStep from "./ConfirmStep";
+
+function ScheduleForm() {
+  // return <CalendarStep />;
+  return <ConfirmStep />;
+}
+
+export default ScheduleForm;
