82a6dc6d4220481632f9446fb31228fb9a5ec9ee
Caio Cezar <caiocezartg@gmail.com>
2023-11-09 01:53:33 -0300

    chore: install react query

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -47,8 +47,8 @@ export default async function handler(
 
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability;
 
-  const startHour = time_start_in_minutes / 60;
-  const endHour = time_end_in_minutes / 60;
+  const startHour = time_start_in_minutes / 60; //10hrs example
+  const endHour = time_end_in_minutes / 60; // 18hrs example
 
   const possibleTimesArray = Array.from({ length: endHour - startHour }).map(
     (_, i) => {
